8edbd471bb075281dc6575e24e3387de5e2b21c5
Tristan Strathearn <tristan.strathearn@khemistry.com.au>
2019-03-20 10:11:24 +1000

    Test 6

--- next.config.js
+++ next.config.js
@@ -1,3 +1,20 @@
+const webpack = require('webpack');
+const { reduce } = require('ramda');
+
+const EXPOSED_ENVS = [
+  'REDIS_TOKEN',
+];
+
 module.exports = {
-  target: 'serverless'
+  target: 'serverless',
+  webpack: (config, options) => {
+    config.plugins.push(
+      new webpack.DefinePlugin(reduce((obj, env) => ({
+        ...obj,
+        [`process.env.${env}`]: JSON.stringify(process.env[env]),
+      }), {}, EXPOSED_ENVS)),
+    );
+
+    return config;
+  },
 }
